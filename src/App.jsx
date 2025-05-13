5cb57d0164806a14210826800f9cc5e24eefbbb0
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-05-13 12:42:44 +0300

    unicafe is done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {average.toFixed(1)}</p>
+      <p>Positive {positive.toFixed(1)}%</p>
+    </div>
+  )
+}
+
+export default App
